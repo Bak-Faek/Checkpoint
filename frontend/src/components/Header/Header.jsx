539bf4eb66a425ebab7cc68ef52d28c7bd4d7922
Patrick Hoang <hoang.patrick@outlook.com>
2024-02-01 12:10:30 +0100

    ajout navbar et header sur mobile avec lien fonctionnel

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.jpg";
+import profile from "../../assets/connexion.png";
+
+export default function Header() {
+  // const { logout, userData } = useUserContext();
+  // const logOutFromSession = () => {
+  //   logout();
+  // };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.home}>
+        <Link className={styles.svgLink} to="/">
+          <img className={styles.svgLogo} src={logo} alt="logo" />
+        </Link>
+      </div>
+
+      <h1>Candle</h1>
+
+      <div className={styles.favoris}>
+        <Link className={styles.svgLink} to="/login">
+          <img className={styles.svgProfil} src={profile} alt="profile" />
+        </Link>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,9 +1,13 @@
 import Admin from "../components/Admin/Admin";
+import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
 
 function AdminPage() {
   return (
     <div>
+      <Header />
       <Admin />
+      <Navbar />
     </div>
   );
 }
